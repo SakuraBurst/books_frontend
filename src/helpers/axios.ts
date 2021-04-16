62ef9b17c9d437966427ba1be1642e07f202672f
SakuraBurst <colapes2@gmail.com>
2021-04-16 19:29:20 +0300

    axios timeout

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const AxiosInstanse = axios.create({
-  baseURL: "http://localhost:3585",
-  timeout: 1000,
+  baseURL: "http://192.168.0.144:3584",
+  timeout: 15000,
 });
